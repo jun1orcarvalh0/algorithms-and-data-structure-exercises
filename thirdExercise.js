e9590299b067a55ddbf38fef5cf63ad3621937d7
Junior <antoniomarcosdecj18@gmail.com>
2023-04-17 19:17:39 -0300

    [fix] Refactoring the third exercise

--- thirdExercise.js
+++ thirdExercise.js
@@ -1,21 +1,16 @@
-function generateRandomID(defaultFormat = "XXXX-AAAA-BBBB-CCCC") {
+function generateRandomID() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let id = '';
 
-  for (let i = 0; i < defaultFormat.length; i++) {
-    if (defaultFormat[i] === 'X') {
-      for (let j = 0; j < 4; j++) {
-        id += characters.charAt(Math.floor(Math.random() * characters.length));
-      }
-    } else if (defaultFormat[i] === 'A' || defaultFormat[i] === 'B' || defaultFormat[i] === 'C') {
-      for (let j = 0; j < 4; j++) {
-        id += characters.charAt(Math.floor(Math.random() * characters.length));
-      }
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      id += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    if (i < 3) {
       id += '-';
-    } else {
-      id += defaultFormat[i];
     }
   }
 
   return id;
 }
+console.log(generateRandomID())
